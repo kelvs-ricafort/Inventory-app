2edecf702865e4488d8efacbac0dbfd74990f2fd
kelvs-ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2023-03-16 22:57:23 +0800

    Initial Commit

--- app/src/main/java/com/newtechieblog/wordpress/inventoryapp/InventoryApplication.kt
+++ app/src/main/java/com/newtechieblog/wordpress/inventoryapp/InventoryApplication.kt
@@ -1,6 +1,8 @@
 package com.newtechieblog.wordpress.inventoryapp
 
 import android.app.Application
+import com.newtechieblog.wordpress.inventoryapp.data.ItemRoomDatabase
 
 class InventoryApplication : Application() {
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
 }
